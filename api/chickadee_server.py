b7c6716e874014da0e0d4172f3ad5a760313cdcb
tunamako <inedibledelicacies@gmail.com>
2017-12-01 00:58:54 -0500

    GET /birds working

--- api/chickadee_server.py
+++ api/chickadee_server.py
@@ -1,33 +1,59 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from socketserver import ThreadingMixIn
-import mysql.connector as sql
+from flask import Flask, request, abort, jsonify
+from flask_mysqldb import MySQL
 import json
+import datetime
+from flask.json import JSONEncoder
 
-class RequestHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		
-		self.send_response(200)
-		self.send_header('Content-type', 'application/json')
-		self.end_headers()
+client_whitelist = [
+	'127.0.0.1',
+	'198.110.204.9' #euclid
+]
 
-	def do_PUT(self):
-		self.send_response(200)
+app = Flask(__name__)
+mysql = MySQL()
 
-	def do_POST(self):
-		self.send_response(200)
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'chickadeesTesting'
 
-	def do_DELETE(self):
-		self.send_response(200)
+with app.app_context():
+	mysql.init_app(app)
 
 
-class ChickadeeServer(ThreadingMixIn, HTTPServer):
-	_client_whitelist = [
-		'127.0.0.1',
-		'198.110.204.9' #euclid
-	]
-	def verify_request(self, request, addr):
-		return addr[0] in self._client_whitelist
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime.date) or isinstance(obj, datetime.timedelta) or isinstance(obj, datetime.datetime):
+                return str(obj)
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
+app.json_encoder = CustomJSONEncoder
+
+
+
+@app.before_request
+def limit_remote_addr():
+	if request.remote_addr not in client_whitelist:
+		abort(403)
+
+@app.route("/api/birds",methods=['GET', 'POST'])
+def birds():
+	cur = mysql.connection.cursor()
+	if request.method == 'GET':
+		cur.execute('''SELECT * FROM birds''')
+
+	data = [dict((cur.description[i][0], value)
+    	for i, value in enumerate(row)) for row in cur.fetchall()]
+
+	return jsonify(data)
+
 
 if __name__ == "__main__":
-	server = ChickadeeServer(("localhost", 8127), RequestHandler)
-	server.serve_forever()
+	app.run()
+
