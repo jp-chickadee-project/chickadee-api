825525a8654e866803910d85658fff515f0fd4d6
tunamako <inedibledelicacies@gmail.com>
2018-03-14 16:02:08 -0400

    added script to load visit zipfiles from bio people

--- manage.py
+++ manage.py
@@ -3,7 +3,12 @@ from flask_script import Manager
 import mysql.connector as sql
 
 import unittest
-
+import os
+import sys
+import json
+import time
+import datetime
+import zipfile
 from chickadee_api import create_app
 
 manager = Manager(create_app)
@@ -46,6 +51,51 @@ def createdb(username="", password="", dbname="chickadees"):
 	cnx.close()
 
 	print("Database created")
+
+@manager.command
+def loadvisits(visitzip="", db="", username="", password=""):
+	if not visitzip:
+		print("Visit zipfile location not entered, exiting")
+		return
+
+	cnx = sql.connect(
+		user=username,
+		password=password,
+		database=db
+	)
+	cursor = cnx.cursor(buffered=True)
+	cnx.get_warnings=True
+
+	visitzip = zipfile.ZipFile(visitzip)
+
+	for f in visitzip.infolist():
+		feederID = f.filename.split("/")[-1][:4]
+		if feederID == "RFID":
+			continue
+
+		print("Loading file:  " + f.filename)
+		for line in visitzip.open(f).readlines()[2:]:
+			line = line.decode("utf-8").rstrip("\r\n").split(" ")
+			rfid = line[0]
+
+			date = str(line[1][:-2]) + "20" + str(line[1][-2:])
+			timestamp = date + " " + line[2][:-1]
+			timestamp = time.mktime(datetime.datetime.strptime(timestamp, "%m/%d/%Y %H:%M:%S").timetuple())
+
+			cursor.execute("SELECT bandCombo FROM birds WHERE rfid = '{0}';".format(rfid))
+			bandCombo = cursor.fetchone()[0]
+
+			command = (
+				"INSERT INTO visits (rfid, visitTimestamp, feederID, bandCombo, temperature, mass) "
+				"VALUES ('{0}', '{1}', '{2}', '{3}', 0, 0);".format(rfid, timestamp, feederID, bandCombo))
+			cursor.execute(command)
+
+		warnings = cursor.fetchwarnings()
+		if warnings: print(warnings)
+
+		print("Completed file: " + f.filename)
+	cnx.commit()
+	print("Visits loaded")
 
 if __name__ == "__main__":
 	manager.run()
